99469f8a0ece95c1b5623489afb819bc86f708bf
kartiktiwari216 <kartiktiwari216@gmail.com>
2026-03-07 17:26:59 +0530

    Update Task.java

--- taskey/src/main/java/com/app/taskey/models/Task.java
+++ taskey/src/main/java/com/app/taskey/models/Task.java
@@ -5,16 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Data
 public class Task {
     @Id
-      @GeneratedValue (strategy  = GenerationType.AUTO)
-    private  long id;
-    private  String title;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    private String title;
     private boolean completed;
-
 }
